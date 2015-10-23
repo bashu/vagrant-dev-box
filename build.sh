e29a902977fe1e200a616aa4d8e0621992a1f62f
bashu <basil.shubin@gmail.com>
2015-10-23 18:50:32 +0600

    fixed shebang

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # to build package.box:
 vagrant destroy -f
--- vagrant/purge.sh
+++ vagrant/purge.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Credits to:
 #  - http://vstone.eu/reducing-vagrant-box-size/
